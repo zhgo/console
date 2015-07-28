86a20532f769bb59e1a18220068f5d1497c5b860
liudng <liudng@gmail.com>
2015-07-28 17:36:09 +0800

    Improve test.

--- util_test.go
+++ util_test.go
@@ -23,3 +23,35 @@ func TestCamelcaseToUnderscore(t *testing.T) {
 		t.Errorf("methodToPath failure: %#v", path)
 	}
 }
+
+func TestParseText(t *testing.T) {
+	cmd := "ls.exe -l"
+	sli := ParseText(cmd)
+	if len(sli) != 2 || sli[0] != "ls.exe" || sli[1] != "-l" {
+		t.Errorf("ParseText failure: %#v", cmd)
+	}
+
+	cmd = "cd {homePath}/gocode/src"
+	sli = ParseText(cmd)
+	if len(sli) != 2 || sli[0] != "cd" || sli[1] != "{homePath}/gocode/src" {
+		t.Errorf("ParseText failure: %#v", cmd)
+	}
+
+	cmd = "run cd {basePath}/usr/local/apache24"
+	sli = ParseText(cmd)
+	if len(sli) != 3 || sli[0] != "run" || sli[1] != "cd" || sli[2] != "{basePath}/usr/local/apache24" {
+		t.Errorf("ParseText failure: %#v", cmd)
+	}
+
+	cmd = "srv httpd.exe"
+	sli = ParseText(cmd)
+	if len(sli) != 2 || sli[0] != "srv" || sli[1] != "httpd.exe" {
+		t.Errorf("ParseText failure: %#v", cmd)
+	}
+
+	cmd = "async 'D:/Program Files/Sublime Text 3/sublime_text.exe'"
+	sli = ParseText(cmd)
+	if len(sli) != 2 {
+		t.Errorf("ParseText failure: %#v", cmd)
+	}
+}
