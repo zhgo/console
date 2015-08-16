431e9fecb11d7e5ed5a466ccb59b0d6f783f839f
liudng <liudng@gmail.com>
2015-08-16 09:52:09 +0200

    Path convertion functions

--- util.go
+++ util.go
@@ -123,26 +123,46 @@ func Arguments(app string) (string, string) {
 
 // for example: transfer browse_by_set to BrowseBySet
 func UnderscoreToCamelcase(str string) string {
+	return LowerToCamelcase(str, "_")
+}
+
+// for example: transfer BrowseBySet to browse_by_set
+func CamelcaseToUnderscore(str string) string {
+	return CamelcaseToLower(str, "_")
+}
+
+func LowerToCamelcase(str string, sp string) string {
 	var method string
-	sli := strings.Split(str, "_")
+	sli := strings.Split(str, sp)
 	for _, v := range sli {
 		method += strings.Title(v)
 	}
 	return method
 }
 
-// for example: transfer BrowseBySet to browse_by_set
-func CamelcaseToUnderscore(str string) string {
-	var words []string
+func CamelcaseToLower(str string, sp string) string {
+	return strings.Join(CamelcaseToLowerSlice(str, -1), sp)
+}
 
+func CamelcaseToLowerSlice(str string, limit int) []string {
+	var words []string
 	l := 0
+	i := 1
+
 	for s := str; s != ""; s = s[l:] {
 		l = strings.IndexFunc(s[1:], unicode.IsUpper) + 1
 		if l < 1 {
 			l = len(s)
 		}
 		words = append(words, strings.ToLower(s[:l]))
+
+		if limit > 0 && limit >= i {
+			words = append(words, strings.ToLower(s))
+			break
+		}
+
+		i++
 	}
 
-	return strings.Join(words, "_")
+	return words
 }
